79c7fec1d73182c5873afad55b8a1115ca2fa327
ThienRabbit <100837623+ThienRabbit@users.noreply.github.com>
2024-02-04 19:11:45 +0700

    Update Autobounty2.lua

--- Autobounty2.lua
+++ Autobounty2.lua
@@ -1,35 +1,35 @@
-getgenv().SpamSkill = true
+getgenv().SpamSkill = false
 getgenv().AutoUseRaceV3 = true
 getgenv().AutoUseRacev4 = true
-getgenv().SpamSkillOnRaceV4 = true
+getgenv().SpamSkillOnRaceV4 = false
 getgenv().Invisible = true
-getgenv().InCombatNoReset = false
+getgenv().InCombatNoReset = true
 getgenv().Team = "Pirates" -- Marines
-getgenv().TweenSpeed = 300 -- 350 max or Get Tp Back
+getgenv().TweenSpeed = 350 -- 350 max or Get Tp Back
  getgenv().Setting = { -- Select Weapon, Self Explain
         ["Melee"] = {["Enable"] = true,["Delay"] = 2,
           ["Skills"] = {
-            ["Z"] = {["Enable"] = true,["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
+            ["Z"] = {["Enable"] = true,["HoldTime"] = 0.3,["TimeToNextSkill"] = 0,},
             [ "X"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
-            ["C"] = {["Enable"] = true,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
+            ["C"] = {["Enable"] = true,["HoldTime"] = 0.1, ["TimeToNextSkill"] = 0,},
             },
         },
-        ["Blox Fruit"] = {["Enable"] = true, ["Delay"] = 2.5,
+        ["Blox Fruit"] = {["Enable"] = false, ["Delay"] = 4,
             ["Skills"] = {
-                ["Z"] = {["Enable"] = true, ["HoldTime"] = 2, ["TimeToNextSkill"] = 0,},
+                ["Z"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
                 ["X"] = { ["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
                 ["C"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0, },
                 ["V"] = { ["Enable"] = true, ["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
                 ["F"] = {["Enable"] = false,["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
             },
         },
-        ["Sword"] = { ["Enable"] = false, ["Delay"] = 2,
+        ["Sword"] = { ["Enable"] = true, ["Delay"] = 0.2,
             ["Skills"] = {
-                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,},
-                ["X"] = {["Enable"] = true, ["HoldTime"] = 0.5, ["TimeToNextSkill"] = 0,},
+                ["Z"] = {["Enable"] = true,  ["HoldTime"] = 0,["TimeToNextSkill"] = 0,},
+                ["X"] = {["Enable"] = true, ["HoldTime"] = 0, ["TimeToNextSkill"] = 0,},
             },
         },
-        ["Gun"] = {["Enable"] = true, ["Delay"] = 2,
+        ["Gun"] = {["Enable"] = false, ["Delay"] = 2,
             ["Skills"] = {
                 ["Z"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,},
                 ["X"] = {["Enable"] = true,["HoldTime"] = 0.5,["TimeToNextSkill"] = 0,
